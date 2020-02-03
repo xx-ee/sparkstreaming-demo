56b675cf6e7b99ac4559b03807ae602105f57269
xiedong <xiedong@rmitec.cn>
2020-02-03 18:53:52 +0800

    ok

---
+++ src/main/scala/SparkStreamingDemo.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming._
+
+object SparkStreamingDemo {
+  def main(args: Array[String]): Unit = {
+//    val conf=new SparkConf().setMaster("spark://192.168.154.10:7077").setAppName("SparkStreamingDemo");
+    val conf=new SparkConf().setAppName("SparkStreamingDemo");
+    val sc=new SparkContext(conf)
+    val ssc = new StreamingContext(sc,Seconds(5))
+    val lines = ssc.textFileStream("file:///home/pros/stream")
+    val words = lines.flatMap(_.split(" "));
+    val wordCounts = words.map((_,1)).reduceByKey(_+_);
+    wordCounts.print();
+    ssc.start();
+
+    ssc.awaitTermination()  //保持SparkStreaming一直开启，直到用户手动中断
+  }
+}
