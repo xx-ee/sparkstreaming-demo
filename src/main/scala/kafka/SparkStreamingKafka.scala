56f2cb19b15aef6fb927c4669d5560d6b6d067ed
xiedong <xiedong@rmitec.cn>
2020-02-06 20:07:44 +0800

    ``

--- src/main/scala/kafka/SparkStreamingKafka.scala
+++ src/main/scala/kafka/SparkStreamingKafka.scala
@@ -14,27 +14,18 @@ object SparkStreamingKafka {
     //--如果只有一个线程，则消费不到Kafka数据
     val conf=new SparkConf().setMaster("local[2]").setAppName("streamkafka")
     val sc=new SparkContext(conf)
-
     val ssc=new StreamingContext(sc,Seconds(10))
-
     //--指定zk集群列表
     val zkHosts="192.168.2.10:2181,192.168.2.11:2181,192.168.2.12:2181"
     //--指定消费者组名
     val group="gp1"
-
     //--指定消费的主题，key是主题名，value是消费的线程数
     val topics=Map("music"->1,"enbook"->1)
-
-
     //--通过工具类去消费Kafka数据
     val data=KafkaUtils.createStream(ssc, zkHosts, group, topics)
       .map{x=>x._2}
-
-
     data.print
-
     ssc.start
-
     ssc.awaitTermination()
 
   }
